35b94bc3f7e8637932bd051ec50c3e6569bd0ca0
pixcai <pixcai@163.com>
2017-12-14 11:03:51 +0800

    Add scss support

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import Header from '../components/Header'
 export default class Index extends React.Component {
 	
 	render() {
-		return <Header />
+		return (
+      <div className="index">
+        {/* you can write scss inside <style jsx> */}
+        <style jsx>{`.index { color: green; }`}</style>
+        <Header />
+      </div>
+    )
 	}
 }
