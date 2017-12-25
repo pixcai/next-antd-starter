2f94347f39ff409ba4c8405dcc1b0de99ee8872c
pixcai <pixcai@163.com>
2017-12-25 11:11:00 +0800

    Add charset

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default class GlobalDocument extends Document {
     return (
       <html lang={process.env.LANGUAGE}>
         <Head>
+          <meta charSet={process.env.CHARSET} />
           <meta name="viewport" content="width=device-width, initial-scale=1" /> 
           <link rel="shortcut icon" type="images/x-icon" href="/static/favicon.png" />
           <style dangerouslySetInnerHTML={{ __html: styles }} />
